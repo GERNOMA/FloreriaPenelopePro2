2f00cb87f48918e12517110dce472792cec6981a
GERNOMA <leomoralesvicente@gmail.com>
2023-08-05 18:34:33 -0300

    guardando

---
+++ app/buy/page.tsx
@@ -0,0 +1,18 @@
+//'use client';
+import { useState } from "react";
+import axios from "axios";
+import { Inter } from "next/font/google";
+import { get } from "http";
+import Link from "next/link";
+import Image from 'next/image';
+
+export default async function ProductRender({ params }: any) {
+
+    return (
+    <div>
+        <main>
+            <p>aahhaa</p>
+        </main>
+    </div>
+    );
+}
