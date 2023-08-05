1f01062f67018a8050da83a94e0cf4726abe7847
GERNOMA <leomoralesvicente@gmail.com>
2023-08-05 20:04:48 -0300

    guardando

--- app/api/auth/options.ts
+++ app/api/auth/options.ts
@@ -2,6 +2,9 @@
 
     export const options = {
         providers: [
-
+           /* GoogleProvider({
+                clientId: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            })*/
         ],
     };
