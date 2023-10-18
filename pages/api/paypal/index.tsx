364a0e3a82d1f59eb9568c7f5d0946806e77c6dd
GERNOMA <leomoralesvicente@gmail.com>
2023-10-18 20:48:26 -0300

    guardando

--- pages/api/paypal/index.tsx
+++ pages/api/paypal/index.tsx
@@ -4,7 +4,9 @@ const configureEnvironment = function () {
   const clientId = process.env.PAYPAL_CLIENT_ID
   const clientSecret = process.env.PAYPAL_CLIENT_SECRET
 
-  return new paypal.core.LiveEnvironment(clientId, clientSecret)
+  return process.env.NODE_ENV == 'production'
+    ? new paypal.core.LiveEnvironment(clientId, clientSecret)
+    : new paypal.core.LiveEnvironment(clientId, clientSecret)
 }
 
 const client = function () {
