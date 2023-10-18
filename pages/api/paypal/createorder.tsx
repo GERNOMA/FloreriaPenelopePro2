823b5627d25ebcf6464490e3df9779ba8936d34b
GERNOMA <leomoralesvicente@gmail.com>
2023-10-18 20:21:12 -0300

    guardando

--- pages/api/paypal/createorder.tsx
+++ pages/api/paypal/createorder.tsx
@@ -9,14 +9,11 @@ export default async function Handler(req: any, res: any) {
     return res.status(400).json({success: false, message: "Please Provide order_price And User ID"})
 
   try{
-    console.log('t1111');
     const PaypalClient = client()
-    console.log('t2222');
     //This code is lifted from https://github.com/paypal/Checkout-NodeJS-SDK
     const request = new paypal.orders.OrdersCreateRequest()
-    console.log('t3333');
+
     request.headers['prefer'] = 'return=representation'
-    console.log('t44444');
     request.requestBody({
       intent: 'CAPTURE',
       purchase_units: [
@@ -28,15 +25,12 @@ export default async function Handler(req: any, res: any) {
         },
       ],
     })
-    console.log('t55555');
     const response = await PaypalClient.execute(request)
-    console.log('t666666');
+    
     if (response.statusCode !== 201) {
       console.log("RES: ", response)
       return res.status(500).json({success: false, message: "Some Error Occured at backend"})
     }
-
-    console.log('1222  ' + (response.result['id']) );
 
     // Your Custom Code for doing something with order
     // Usually Store an order in the database like MongoDB 
--- pages/api/paypal/index.tsx
+++ pages/api/paypal/index.tsx
@@ -1,8 +1,8 @@
 const paypal = require('@paypal/checkout-server-sdk');
 
 const configureEnvironment = function () {
-  const clientId = 'AcoJt7M_rFHEa0bn9nP9ZlKOviKFZprXWCkeG2K-ra65Amnq2c89_yVOuNNAiaWdOKH8Q5Jg8XDVRjhM'//process.env.PAYPAL_CLIENT_ID
-  const clientSecret = 'ENz8VaS03ngvPWnO_NQ24lyzkxpngHJ1-3l1M6ib8eT0z8ptMmbCn1LIRrTWjK1ulIAXO8bOS3lVPcn1'//process.env.PAYPAL_CLIENT_SECRET
+  const clientId = process.env.PAYPAL_CLIENT_ID
+  const clientSecret = process.env.PAYPAL_CLIENT_SECRET
 
   return process.env.NODE_ENV === 'production'
     ? new paypal.core.LiveEnvironment(clientId, clientSecret)
